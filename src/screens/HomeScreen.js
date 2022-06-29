c4a0207ec416434a92f6d7a35f3325a15089accb
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-06-29 15:52:10 +0300

    Search Bar section

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native"
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native"
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import COLORS from "../data/colors"
 
@@ -12,6 +12,15 @@ const HomeScreen = () => {
                 </View>
                 <Icon name="shopping-cart" size={28}/>
             </View>
+            <View style={{ marginTop: 30, flexDirection: 'row'}}>
+                <View style={style.searchContainer}>
+                    <Icon name="search" size={25} style={{ marginLeft: 20 }}/>
+                    <TextInput placeholder="Search" style={ style.input}/>
+                </View>
+                <View style={style.sortBtn}>
+                    <Icon name="sort" size={30} color={ COLORS.white}/>
+                </View>
+            </View>
         </SafeAreaView>
     )
 } 
@@ -21,6 +30,29 @@ const style = StyleSheet.create({
         marginTop: 20,
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+    searchContainer: {
+        height: 50,
+        backgroundColor:COLORS.light,
+        borderRadius: 10,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    input : {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: COLORS.dark,
+        flex: 1
+    },
+    sortBtn: {
+        marginLeft: 10,
+        height: 50,
+        width: 50,
+        backgroundColor: COLORS.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
     }
 })
 export default HomeScreen
